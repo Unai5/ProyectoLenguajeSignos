6af9da090aa7ff8a42d4b68205bac5de134c5150
Alex Meleiro <alexmele428@gmail.com>
2023-11-03 13:08:29 +0100

    FOTOS

--- app/src/main/java/org/cuatrovientos/diccionariolenguajesignos/adapters/RecyclerDataAdapter.java
+++ app/src/main/java/org/cuatrovientos/diccionariolenguajesignos/adapters/RecyclerDataAdapter.java
@@ -1,5 +1,6 @@
 package org.cuatrovientos.diccionariolenguajesignos.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -7,65 +8,85 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.cuatrovientos.diccionariolenguajesignos.R;
-import org.cuatrovientos.diccionariolenguajesignos.R;
+import org.cuatrovientos.diccionariolenguajesignos.model.Categoria;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.RealmResults;
 
 public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.RecyclerDataHolder> {
 
     ArrayList<Categoria> listaCategorias;
 
-    public RecyclerDataAdapter(ArrayList<Categoria> listaCategorias) {
-        this.listaNombresCategorias = listaNombresCategorias;
-    }
+    public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.RecyclerDataHolder> {
+        private List<Categoria> list;
+        private OnItemClickListener listener;
 
-    @NonNull
-    @Override
-    public RecyclerDataAdapter.RecyclerDataHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerDataAdapter.RecyclerDataHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    public class RecyclerDataHolder extends RecyclerView.ViewHolder {
-
-        ImageView iv;
-        TextView tv;
-
-        public RecyclerDataHolder(@NonNull View itemView) {
-            super(itemView);
-            iv = itemView.findViewById(R.id.imageView);
-            tv = itemView.findViewById(R.id.textView);
+        public RecyclerDataAdapter(RealmResults<Categoria> list, OnItemClickListener listener){
+            this.list = list;
+            this.listener = listener;
+        }
+        @NonNull
+        @Override
+        public RecyclerDataHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_items,parent,false);
+            return new RecyclerDataHolder(view);
         }
 
-        public void assignData(Categoria categoria, final AdapterView.OnItemClickListener onItemClickListener) {
-            //TODO: Importar clase categoría, y actualizar el nombre de los metodos setIdImagen y getTexto
-            iv.setImageResource(categoria.getIdImagen());
-            tv.setText(categoria.getTexto());
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerDataHolder holder, int position) {
+            holder.assignData(list.get(position),listener);
+        }
 
-            /*
-            itemView.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
 
-                @Override
-                public void onClick(View view) {
-                    onItemClickListener.onItemClick(elementoLista,getAdapterPosition());
+        public void filter(String string) {
+            list.clear();
+            if(string.isEmpty()){
+                list.addAll(listCopia);
+            } else{
+                string = string.toLowerCase();
+                for(Categoria item: listCopia){
+                    if(item.getTitulo().toLowerCase().contains(string)){
+                        list.add(item);
+                    }
                 }
-            });
-            */
-
+            }
         }
 
+        public class RecyclerDataHolder extends RecyclerView.ViewHolder {
+            TextView  tw;
+            ImageView img;
+            public RecyclerDataHolder(@NonNull View itemView) {
+                super(itemView);
+                img = itemView.findViewById(R.id.imgPortada);
+                tw = itemView.findViewById(R.id.txtTitle);
+            }
+
+            public void assignData(Imagenes objeto, OnItemClickListener listener) {
+
+                tw.setText(objeto.getTitulo());
+                tw.setBackgroundResource(objeto.getColorTexto());
+                img.setImageResource(objeto.getImagen());
+                img.setBackgroundResource(objeto.getColorImagen());
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        listener.onItemClick(objeto, getAdapterPosition());
+                    }
+                });
+            }
+        }
+
+        public interface OnItemClickListener{
+            void onItemClick(Imagenes objeto,int position);
+        }
     }
-}
+
