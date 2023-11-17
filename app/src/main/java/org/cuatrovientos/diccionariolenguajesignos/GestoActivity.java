ba30c24b29b80939b4c9b0da2a733664aebd50b1
Alex Meleiro <alexmele428@gmail.com>
2023-11-17 14:25:12 +0100

    activity_gesto2

--- app/src/main/java/org/cuatrovientos/diccionariolenguajesignos/GestoActivity.java
+++ app/src/main/java/org/cuatrovientos/diccionariolenguajesignos/GestoActivity.java
@@ -1,6 +1,7 @@
 package org.cuatrovientos.diccionariolenguajesignos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.location.GnssAntennaInfo;
 import android.os.Bundle;
@@ -8,24 +9,56 @@ import android.os.Bundle;
 import org.cuatrovientos.diccionariolenguajesignos.R;
 import org.cuatrovientos.diccionariolenguajesignos.adapters.RecyclerDataAdapter;
 import org.cuatrovientos.diccionariolenguajesignos.adapters.RecyclerPalabraAdapter;
+import org.cuatrovientos.diccionariolenguajesignos.model.Palabra;
 
 import android.content.Intent;
 
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import io.realm.Realm;
 
 public class GestoActivity extends AppCompatActivity {
-    Button buttonVolver = findViewById(R.id.buttonVolver);
+
+    ImageView imgView;
+
+    TextView txtNombreGesto, txtCategoria;
+    Button buttonVolver ;
+    Realm realm;
+
+    private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesto);
+        realm = Realm.getDefaultInstance();
+        Bundle bundle = getIntent().getExtras();
+        int id = bundle.getInt("id");
+        Palabra palabra = realm.where(Palabra.class).equalTo("id",id).findFirst();
 
 
-        buttonVolver.setOnClickListener(v -> {
-            Intent intent = new Intent(GestoActivity.this, MainActivity.class);
-            startActivity(intent);
-        });
 
+        imgView = findViewById(R.id.imgPalabra);
+        txtNombreGesto = findViewById(R.id.textViewPalabra);
+        txtCategoria = findViewById(R.id.textViewCategoria);
+        buttonVolver = findViewById(R.id.buttonVolver);
+        intent = new Intent(GestoActivity.this, SecondActivity.class);
+
+        txtNombreGesto.setText(palabra.getPalabra());
+        txtCategoria.setText(palabra.getCategoria().getNombre());
+        imgView.setImageResource(palabra.getImagen());
+
+        buttonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        }   );
 
 
     }
